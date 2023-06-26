adcea997cbfeea7effd1177fa52ba3f234f7f2f9
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2023-06-26 20:48:10 +0530

    Create package.json file. And install packages Example(packages are save in Dependaencies on package.json files)

--- index.js
+++ index.js
@@ -5,3 +5,24 @@ http.createServer((req, resp)=>{
     resp.write(JSON.stringify('Web Developer ROHIT, NODE User'));
     resp.end();
 }).listen(4500);
+
+
+// Create package.json file using node.js
+
+    // using command line
+        // >npm init
+        // package name: (node_teto) node-tetorial
+        // version: (1.0.0)
+        // description: practise for Creating package.json file
+        // entry point: (index.js)
+        // test command:
+        // git repository: https://github.com/Rohit3920/NodeTetorial
+        // keywords: step by step code
+        // author: Rohit Nittawadekar
+        // license: (ISC)
+
+    // Install packages 
+        // > npm i colors
+            //install some packages to create packafe-lock.json file and node module folder
+            //you don't push node mosule folder in repo
+            //this folder hava ignore in git 
