6ae1f3b82c46faa61f4a87b654407ad60ce43e6c
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2023-06-26 22:11:54 +0530

    Simple APL with static data on module export

---
+++ data.js
@@ -0,0 +1,8 @@
+
+const data =[
+    {name : 'rohit nittawadekar', email : 'rohitnittawadekar07@gmail.com'},
+    {name : 'nilesh sawant', email : 'nileshsawant00@gmail.com'},
+    {name : 'cedant ', email : 'vedant170902@gmail.com'},
+]
+
+module.exports = data;
--- index.js
+++ index.js
@@ -1,28 +1,8 @@
-const http = require('http')
+const http = require('http');
+const data = require('./data')
 
 http.createServer((req, resp)=>{
-    resp.writeHead(200, {'content-type' : 'application/json'});
-    resp.write(JSON.stringify('Web Developer ROHIT, NODE User'));
+    resp.writeHead(200, {'content-type': 'application/json'});
+    resp.write(JSON.stringify(data.name));
     resp.end();
-}).listen(4500);
-
-
-// Create package.json file using node.js
-
-    // using command line
-        // >npm init
-        // package name: (node_teto) node-tetorial
-        // version: (1.0.0)
-        // description: practise for Creating package.json file
-        // entry point: (index.js)
-        // test command:
-        // git repository: https://github.com/Rohit3920/NodeTetorial
-        // keywords: step by step code
-        // author: Rohit Nittawadekar
-        // license: (ISC)
-
-    // Install packages 
-        // > npm i colors
-            //install some packages to create packafe-lock.json file and node module folder
-            //you don't push node mosule folder in repo
-            //this folder hava ignore in git 
+}).listen(5000);
