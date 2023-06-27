ec75f1bd0b3c58fcae9ca08405b4162e06c197fa
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2023-06-27 19:39:37 +0530

    Create multiple file list for spacific folder path

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-const fs = require('fs')
-const input = process.argv;
+const fs = require('fs');
+const path = require('path');
 
-if (input[2] == 'add'){
-    fs.writeFileSync(input[3], input[4])    //Create file using Input from command line
-}else if(input[2] == 'remove'){
-    fs.unlinkSync(input[3])         //Delete file using Input from command line
-}else{
-    console.log('Invalid Input')
+const dirPath = path.join(__dirname, 'files')
+
+for (i = 0; i < 5; i++) {
+    fs.writeFileSync(`${dirPath}/hello${i}.txt`, `this file name is hello${i}.txt`)
 }
-
-
-
-
-// const fs = require('fs')
-// const input = process.argv;
-
-// fs.writeFileSync('simple.txt', `my name is :- ${input[2]}`)
