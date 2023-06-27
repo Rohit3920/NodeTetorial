2570cffd401031c15c54eb953c3ea4edaabd387c
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2023-06-27 20:57:43 +0530

    Counting no of  files for dirPath

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const path = require('path');
 
 const dirPath = path.join(__dirname, 'files')
 
-for (i = 0; i < 5; i++) {
-    fs.writeFileSync(`${dirPath}/hello${i}.txt`, `this file name is hello${i}.txt`)
-}
+// for (i = 0; i < 5; i++) {
+//     fs.writeFileSync(`${dirPath}/hello${i}.txt`, `this file name is hello${i}.txt`)
+// }
+
+fs.readdir(dirPath, (err, file)=>{
+    file.forEach((item)=>{
+        console.log(item)
+    })
+})      //Counting files for dirPath
