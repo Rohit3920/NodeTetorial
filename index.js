6b226106fc832a80708d147713598de534812410
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2023-06-29 19:24:10 +0530

    Make a HTML page and load files using node js

--- index.js
+++ index.js
@@ -1,41 +1,11 @@
 const express = require('express');
+const path = require('path')
+
 const App = express();
 
-App.get("", (req, resp) => {
+const pubPath = path.join( __dirname, 'Public')
 
-    resp.send(`
-    <h1>Home page</h1>
-    <p>this is a home page using <strong>Render</strong> HTML in node Js</p>
-    <a href="/about" >Go to About</a>
-    <a href="/data" >Show JSON data</a>
-    `)
-});
+App.use(express.static(pubPath))
 
-App.get("/about", (req, resp) => {
-
-    resp.send(`
-    <h1>About page</h1>
-    <p>this is a about page in node Js</p>
-    <a href="/" >Go to Home</a>
-    `)
-});
-
-App.get("/data", (req, resp) => {
-
-    resp.send([
-        {
-            name: 'Rohit',
-            Gmail: 'rohit@gmail.com'
-        },
-        {
-            name: 'Vedant',
-            Gmail: 'vedant@gmail.com'
-        }
-    ])
-});
 
 App.listen(4622);
-
-
-
-// Rendering HTML and JSON
