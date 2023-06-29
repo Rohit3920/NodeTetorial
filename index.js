a54980be22dfbea49827b0e4d760e3bf59a68a28
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2023-06-29 21:17:34 +0530

    Add comments and some changes for Make folder for HTML files and access it1~

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const path = require('path')
 
 const App = express();
 
-const pubPath = path.join( __dirname, 'Public')
+const pubPath = path.join(__dirname, 'Public')
 
 App.use(express.static(pubPath))
 
 
 App.listen(4622);
+
+// Make folder for HTML files and access it ---->
+// http://localhost:4622   root page active i.e. index.html
+// http://localhost:4622/abput.html
+// http://localhost:4622/help.html
