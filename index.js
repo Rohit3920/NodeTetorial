7e58275ca762226b8da437030ff419f0cff408dc
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2023-06-30 12:47:07 +0530

    Use Templete ENGINE --> (ejs)

--- index.js
+++ index.js
@@ -5,7 +5,16 @@ const App = express();
 
 const pubPath = path.join(__dirname, 'Public')
 
-// App.use(express.static(pubPath))
+App.set('view engine', 'ejs')                           // ejs set in app
+
+App.get('/profile', (_, resp)=>{                         //then use dynamically
+    const user= {
+        name: 'Rohit Nittawadekar',
+        email: "rohitnittawadekar07@gmail.com",
+        prof: "Web Developer- NODE user"
+    }
+resp.render('profile', {user})
+})
 
 App.get('', (_, resp)=>{
     resp.sendFile(`${pubPath}/index.html`)
@@ -26,8 +35,12 @@ App.get('*', (_, resp)=>{
 
 App.listen(4622);
 
-// Remove Extension from URL ---->
-// Make 404 page & use it ---->
-// http://localhost:4622   root page active i.e. index.html
-// http://localhost:4622/abput
-// http://localhost:4622/help
+// Install ejs (engine) templete package ---->
+// setup dynamic routing ---->
+// Male a dynamic page ---->
+
+
+
+// Install ejs package cmd == npm i ejs 
+// set in App 
+// then use 
