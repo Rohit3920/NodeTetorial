15075d8388e700c61e0b9265ca008d62825fd0b0
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2023-06-30 12:06:40 +0530

    Remove Extension from URL. & Make 404 page & use it

--- index.js
+++ index.js
@@ -5,12 +5,29 @@ const App = express();
 
 const pubPath = path.join(__dirname, 'Public')
 
-App.use(express.static(pubPath))
+// App.use(express.static(pubPath))
+
+App.get('', (_, resp)=>{
+    resp.sendFile(`${pubPath}/index.html`)
+})
+
+App.get('/about', (_, resp)=>{
+    resp.sendFile(`${pubPath}/about.html`)
+})
+
+App.get('/help', (_, resp)=>{
+    resp.sendFile(`${pubPath}/help.html`)
+})
+
+App.get('*', (_, resp)=>{
+    resp.sendFile(`${pubPath}/p404.html`)
+})
 
 
 App.listen(4622);
 
-// Make folder for HTML files and access it ---->
+// Remove Extension from URL ---->
+// Make 404 page & use it ---->
 // http://localhost:4622   root page active i.e. index.html
-// http://localhost:4622/abput.html
-// http://localhost:4622/help.html
+// http://localhost:4622/abput
+// http://localhost:4622/help
