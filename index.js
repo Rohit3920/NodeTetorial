6c604d54a5e65eb9a50664b5200e3e8d26e76128
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2023-07-02 16:32:30 +0530

    Route Middleware, Apple Middleware on single route in different js file

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 const express = require('express')
+const reqFilter = require('./Middleware')
 
 const app = express();
 
-const reqFilter = (req, resp, next)=>{
-  if(!req.query.age){
-    resp.send("Please valid age")
-  }else if(req.query.age < 18){
-    resp.send("You can not access the page")
-  }else{
-    next();
-  }
-}
 
-app.use(reqFilter)
 
-app.get('/', (req, resp)=>{
-resp.send("<h1>Hello! this is a HOME page</h1>")
+// app.use(reqFilter)
+
+// Apple Middleware On single Route  ---> user, contact, login pages applied single route
+
+app.get('/', (req, resp) => {
+    resp.send("<h1>Hello! this is a HOME page</h1>")
 })
 
-app.get('/user', (req, resp)=>{
+app.get('/user',reqFilter, (req, resp) => {
     resp.send("<h1>Hello! this is a USER page</h1>")
-    })
+})
 
-    app.listen(5000);
+app.get('/about', (req, resp) => {
+    resp.send("<h1>Hello! this is a ABOUT page</h1>")
+})
+
+app.get('/contact', reqFilter, (req, resp) => {
+    resp.send("<h1>Hello! this is a CONTACT page</h1>")
+})
+
+app.get('/login',reqFilter, (req, resp) => {
+    resp.send("<h1>Hello! this is a LOGIN page</h1>")
+})
+
+app.listen(5000);
