6ad42ab982ad993d86d76daa669abba7f65f8929
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2023-07-04 22:30:23 +0530

    Add mongoDB and connect Node with MongoDB

--- index.js
+++ index.js
@@ -1,31 +1,14 @@
-const express = require('express')
-const reqFilter = require('./Middleware')
-const route = express.Router();
+const { MongoClient } = require('mongodb');
+const url = 'mongodb://localhost:27017';
+const database = 'e-comm';
+const client = new MongoClient(url);
 
-const app = express();
+async function getData() {
+    let result = await client.connect();
+    let db = result.db(database);
+    let collection = db.collection('Products');
+    let response = await collection.find({}).toArray();
+    console.log(response);
+}
 
-route.use(reqFilter)
-
-app.get('/', (req, resp) => {
-    resp.send("<h1>Hello! this is a HOME page</h1>")
-})
-
-route.get('/user', (req, resp) => {
-    resp.send("<h1>Hello! this is a USER page</h1>")
-})
-
-app.get('/about', (req, resp) => {
-    resp.send("<h1>Hello! this is a ABOUT page</h1>")
-})
-
-route.get('/contact', (req, resp) => {
-    resp.send("<h1>Hello! this is a CONTACT page</h1>")
-})
-
-route.get('/login', (req, resp) => {
-    resp.send("<h1>Hello! this is a LOGIN page</h1>")
-})
-
-app.use('/', route)
-
-app.listen(5000);
+getData();
