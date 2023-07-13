b8e513042be9bbaf2d26de22f2281dd1663adc68
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2023-07-13 19:21:57 +0530

    Read data from MongoDB as a async await function

--- index.js
+++ index.js
@@ -11,8 +11,16 @@ async function getData() {
     // console.log(response);
 }
 
-getData().then((resp)=>{
-    resp.find().toArray().then((data)=>{
-        console.log(data)
-    })
-});
+// getData().then((resp)=>{
+//     resp.find().toArray().then((data)=>{
+//         console.log(data)
+//     })
+// });
+
+const main = async () =>{
+    let data = await getData();
+    data = await data.find().toArray();
+    console.log(data)
+}
+
+main();
