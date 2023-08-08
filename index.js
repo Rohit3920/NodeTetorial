2d646f30ce1f24502fb2c0aa5a0df89b8960e023
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2023-08-08 17:31:11 +0530

    Read Data successfully : same changes for Read data to show in the console pannel

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require('mongodb');
-const url = 'mongodb://localhost:27017';
+const url = 'mongodb://127.0.0.1:27017';
 const database = 'e-comm';
 const client = new MongoClient(url);
 
@@ -7,20 +7,10 @@ async function getData() {
     let result = await client.connect();
     let db = result.db(database);
     let collection = db.collection('Products');
-    // let response = await collection.find({}).toArray();
-    // console.log(response);
+    let response = await collection.find({}).toArray();
+    console.log(response);
 }
 
-// getData().then((resp)=>{
-//     resp.find().toArray().then((data)=>{
-//         console.log(data)
-//     })
-// });
+getData();
 
-const main = async () =>{
-    let data = await getData();
-    data = await data.find().toArray();
-    console.log(data)
-}
-
-main();
+// nodemon have start >>cmd :  npm run serve
