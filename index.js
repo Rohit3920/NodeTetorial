1d881ddb54ccf862277a2df1cb1b7cfb00ef1e7e
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2023-08-11 20:41:08 +0530

    Counting & Sorting : counting documents for greater then or equal to RollNo of 5

--- index.js
+++ index.js
@@ -60,11 +60,14 @@ const readDoc = async () => {
         // const result = await Product.find({$and : [{fees : false},{RollNo : 1}]})
         // const result = await Product.find({$or : [{fees : false},{RollNo : 1}]})
         // const result = await Product.find({$nor : [{fees : true},{RollNo : 1}]})
+        // const result = await Product.find({RollNo :{$not : {$gt : 5}}})
 
 
-        const result = await Product.find({RollNo :{$not : {$gt : 5}}})
-
+        const result = await Product.find({RollNo : {$gte : 5}})
         .select({_id: 0, name:1, Phone:1, RollNo:1})
+
+        // counting Documents 
+        .count()
         // .limit(1);
         console.log(result)
     } catch (error) {
