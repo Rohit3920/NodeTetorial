1332e9946404e6c88a5e53ae5eb9df79386305ae
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2023-09-14 17:56:35 +0530

    Node & MongoDB : Update Many data in mongodb using Node

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const dbConnect = require("./mongodb")
 
-const updateOneData = async () => {
+const updateManyData = async () => {
     const doc = await dbConnect();
-    const result = await doc.updateOne({
+    const result = await doc.updateMany({
         name : "swapnil"}, {$set : {fees : false}});
     if(result.acknowledged){
-        console.log("Data Updated successfully")
+        console.log("Many Data Updated successfully")
     }else{
         console.log("Not updated")
     }
 }
-updateOneData();
+updateManyData();
