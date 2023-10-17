02a476e21af9fdda4e15ad43a056ca5731f59d52
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2023-10-17 17:15:05 +0530

    Mongoose and Express : use GET, PUT and DELETE method in API

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 require ("./config");
 const product = require("./product");
 
@@ -12,6 +11,29 @@ app.post("/create", async(req, resp) =>{
     let result = await data.save();
     console.log(req.body);
     resp.send(req.body);
+})
+
+app.get("/list", async (req, resp)=>{
+    const data = await product.find();
+    console.log(data);
+    resp.send(data);
+})
+
+app.delete("/delete/:id", async (req, resp)=>{
+    console.log(req.params);
+const data = await product.deleteOne(req.params)
+    resp.send(data);
+});
+
+app.put("/update/:id", async (req, resp)=>{
+    let data = await product.updateOne(
+        req.params,
+        {
+            $set : req.body
+        }
+    )
+    console.log(req.params)
+    resp.send(req.params)
 })
 
 app.listen(5000);
