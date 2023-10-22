48f61516f59196b2a954041b613123db57233321
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2023-10-22 16:07:41 +0530

    node Js and Multer : send file in project directory

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-const express = require("express");
-const eventEmitter = require("events")
-require ("./config");
-const product = require("./product");
+// install npm-multer package
+// create uploads folder in project directory
+// ser single or multiple file upload (line:20), and set file name (user_file);
+// open postmon and select post method and choose from-data > set Name (user_file) > select "file"> choose file And SEND; 
+
+
+
+const express = require("express")
+const multer = require("multer")
+
 const app = express();
 
-const Event = new eventEmitter();
+const uploadFile =  multer({
+    storage : multer.diskStorage({
+        destination : (req, file, cd)=>{
+            cd(null, "uploads")
+        },
+        filename : (req, file,cd)=>{
+            cd(null, file.fieldname+"-"+Date.now()+'.jpg')
+        }
+    })
+}).single("user_file");
 
-let count = 0;
-
-Event.on("countAPI", (req, resp)=>{
-    count++;
-    console.log("Event called", count);
+app.post("/upload", uploadFile, (req, resp)=>{
+    resp.send("File upload....")
 })
 
-app.get("/", (req, resp)=>{
-    resp.send(`API called `);
-    Event.emit(`countAPI`);
-})
-
-app.listen(5400);
+app.listen(5400)
