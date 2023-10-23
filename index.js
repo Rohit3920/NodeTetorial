822d67ade27be52c3e8137fb93fdbb240dc7eca8
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2023-10-23 15:42:01 +0530

    Node Js and MySQL : POST API method to insert data in database

--- index.js
+++ index.js
@@ -2,13 +2,25 @@ const express = require("express")
 const connMySQL = require("./configMySQL")
 
 const app = express();
+app.use(express.json())
 
 app.get("/", (req, resp) => {
     connMySQL.query("select * from products", (err, result) => {
-       var data = err ? err : result
-        console.log(data)
-        resp.send(data)
+        var data = err ? err : result;
+        console.log(data);
+        resp.send(data);
     })
 })
 
+app.post("/upload", (req, resp) => {
+    const insert = {
+        name: "Rohan",
+        country: "INDAIN",
+    }
+
+    connMySQL.query("INSERT INTO products SET ?", insert, (err, resl, feilds) => {
+        if (err) throw err
+        resp.send(resl);
+    })
+});
 app.listen(5400);
